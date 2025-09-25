8fe5d5cee795b886e95b65ac0e71bab03fa29914
Tufail <tufailbabacoc@gmail.com>
2025-09-25 22:21:27 +0530

    Datatypes

--- Datatypes/script.js
+++ Datatypes/script.js
@@ -1,1 +1,19 @@
-console.log("Hi Tufail");
+// console.log("Hi Tufail");
+// process.stdout.write("chai");
+// console.table({city: "SOPORE"});
+
+// Learning how to print//
+
+let score = 100;
+let name = "Tufailbaba"
+let isLoggedin = false;
+
+
+// objects
+
+let teaTypes = ["lemon tea" , "orange tea"];
+let user = { firstname: "Tufail", lastname: "Baba"};
+console.log(teaTypes);
+
+let getScore = score;
+console.log(getScore);

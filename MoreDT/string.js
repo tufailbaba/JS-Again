fe89926b67384293b4d2c0b154d4b1f6c870ec6f
Tufail <tufailbabacoc@gmail.com>
2025-09-26 14:35:45 +0530

    Some Datatypes

---
+++ MoreDT/string.js
@@ -0,0 +1,41 @@
+//Number
+
+let balance =120;
+let newbalance = new Number(120);
+
+console.log(typeof(newbalance));
+
+
+console.log(typeof(balance));
+
+//Boolean
+
+let isActve = true 
+let isReallyActive = new Boolean(true)
+
+//null and undefined
+
+let firstname;
+console.log(firstname);
+
+//string
+
+let myString = "Hello"
+console.log(myString);
+
+let myStringone = 'Hola'
+let userName = 'Tufail'
+
+let greetMessage = `Hi,Tufail`
+console.log(greetMessage);
+
+let newString = myString + greetMessage; 
+console.log(newString);
+
+
+let anotherString = `Hi ${userName} !`;
+console.log(anotherString);
+
+let add = `Addittion of two number is ${2+2}`
+console.log(add);
+

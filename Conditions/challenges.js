88f2bda92db66907373fc91032b0f564b8e4377c
Tufail <tufailbabacoc@gmail.com>
2025-09-26 22:11:45 +0530

    Challenges in condition

---
+++ Conditions/challenges.js
@@ -0,0 +1,41 @@
+let num1 = 10;
+let num2 = 12;
+
+if (num1 > num2) {
+  console.log("Num1 is Greater than Num2");
+} else {
+  console.log("Num2 is Greater than Num1");
+}
+
+let score = 100;
+
+if (typeof score === "number") {
+  console.log("Its a number");
+} else {
+  console.log("Its not a Number");
+}
+
+let userName1 = "Tufail";
+let userName2 = "baba";
+if (userName1 == userName2) {
+  console.log("Same username");
+} else {
+  console.log("Different Username");
+}
+
+let isTeaReady = true;
+
+if (isTeaReady) {
+  console.log("Tea is ready");
+} else {
+  console.log("Tea is not ready");
+}
+
+let arr = ['item1'];
+console.log(arr.length);
+
+if (arr.length === 0) {
+  console.log("Array is empty");
+} else {
+  console.log("array is not empty");
+}

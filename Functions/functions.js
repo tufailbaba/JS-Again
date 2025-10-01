8973a96caff1d620ea76c93963c426772351eeed
Tufail <tufailbabacoc@gmail.com>
2025-10-01 21:33:50 +0530

    Practicing fns

--- Functions/functions.js
+++ Functions/functions.js
@@ -1,9 +1,9 @@
-function makeTea(typeOfTea) {
+function MakeTea(typeOfTea) {
   return `Making ${typeOfTea}`;
   console.log(test);
 }
 
-let teaOrder = makeTea("Lemon Tea");
+let teaOrder = MakeTea("Lemon Tea");
 // console.log(teaOrder);
 
 function OrderTea(TeaType) {
@@ -18,3 +18,14 @@ let orderConfirmation = OrderTea ("chai")
 const calculateTotal = (price, quantity) => price * quantity
 
 let totalCost = calculateTotal(499 * 100)
+
+function makeTea ( typeOfTea){
+return `makeTea : ${typeOfTea}`;
+
+}
+function processTeaOrder (teafunction){
+    return teafunction('earl grey' )
+}
+
+let order = processTeaOrder(makeTea)
+console.log(order);
